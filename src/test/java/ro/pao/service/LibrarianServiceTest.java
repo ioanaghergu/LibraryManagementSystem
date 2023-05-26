5c7c285a7289270d945df92ce71123b4ca02d746
Ioana Laura Ghergu <ioana-laura.ghergu@s.unibuc.ro>
2023-05-26 15:19:16 +0300

    Added testing for Librarian service

---
+++ src/test/java/ro/pao/service/LibrarianServiceTest.java
@@ -0,0 +1,35 @@
+package ro.pao.service;
+
+import org.junit.jupiter.api.Test;
+import ro.pao.model.Librarian;
+import ro.pao.repository.LibrarianRepository;
+import ro.pao.repository.impl.LibrarianRepositoryImpl;
+
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class LibrarianServiceTest {
+
+    @Test
+    void whenLibrarian_thenIsAdd() throws SQLException {
+
+        Librarian librarian = Librarian.builder()
+                .id(UUID.randomUUID())
+                .name("Alex Nedelcu")
+                .salary(2700.0)
+                .build();
+
+        LibrarianRepository librarianRepository = mock(LibrarianRepositoryImpl.class);
+        LibrarianService librarianService = mock(LibrarianServiceImpl.class);
+
+        when(librarianRepository.getById(any())).thenReturn(Optional.of(librarian));
+
+        assertEquals(Optional.of(librarian), librarianService.getById(any()));
+    }
+}
